2cf274371501dacd4cb3371b578ff150e2644c3c
Daniil <disupov1996@gmail.com>
2018-06-07 16:41:07 +0300

    update

--- src/PriceTable.js
+++ src/PriceTable.js
@@ -8,8 +8,9 @@ class PriceTable extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data_history: [props.rows], // array in which all the state of rows
-            data: props.rows,
+            past_data: [],
+            present_data: props.rows,
+            future_data: [],
             columns: props.columns,
         };
         this.editRow = this.editRow.bind(this);
@@ -31,13 +32,12 @@ class PriceTable extends Component {
                 contentEditable
                 suppressContentEditableWarning
                 onBlur={e => {
-                    const data = [...this.state.data];
-                    data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
-                    this.setState({ data });
-                    this.state.data_history.push(data);
+                    const present_data = JSON.parse(JSON.stringify(this.state.present_data)); // such a JSON bunch we will use to make a DeepCopy
+                    present_data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
+                    this.change_state(present_data);
                 }}
                 dangerouslySetInnerHTML={{
-                    __html: this.state.data[cellInfo.index][cellInfo.column.id]
+                    __html: this.state.present_data[cellInfo.index][cellInfo.column.id]
                 }}
             />
         );
@@ -46,40 +46,57 @@ class PriceTable extends Component {
     actionsRow(cellInfo) {
         return (
             <Button bsStyle="danger" onClick={() => {
-                const data = [...this.state.data];
-                data.splice(cellInfo.index, 1);
-                this.setState({ data });
-                this.state.data_history.push(data);
+                const present_data = JSON.parse(JSON.stringify(this.state.present_data));
+                present_data.splice(cellInfo.index, 1);
+                this.change_state(present_data);
             }}>REMOVE</Button>
         );
+    }
+    // when any changes need to remember the old state and clear the future state
+    change_state(new_state) {
+        const past_data = JSON.parse(JSON.stringify(this.state.past_data));
+        past_data.push(JSON.parse(JSON.stringify(this.state.present_data)));
+
+        this.setState({ past_data: past_data, present_data: new_state, future_data: [] });
     }
     render() {
         return (
             <div className="PriceTable">
                 <ReactTable
-                    data={this.state.data}
+                    data={this.state.present_data}
                     columns={this.state.columns}
                     showPageJump={false}
                     showPagination={false}
                     defaultPageSize={10}
-                    minRows={2}
+                    minRows={this.props.rows.length}
                 />
                 <Row>
                     <Button bsStyle="link" onClick={() => {
-                        const data = [...this.state.data];
-                        data.push({ item: '', cost: '' });
-                        this.setState({ data });
-                        this.state.data_history.push(data);
+                        const present_data = JSON.parse(JSON.stringify(this.state.present_data));
+                        present_data.push({ item: '', cost: '' });
+                        this.change_state(present_data);
                     }}>Add new row</Button>
                 </Row>
                 <Row>
                     <Button bsStyle="primary" onClick={() => {
-                        console.log(this.state.data_history);
+                        if (this.state.past_data.length > 0) {
+                            const { past_data, present_data, future_data } = JSON.parse(JSON.stringify(this.state));
 
+                            const previous = past_data[past_data.length - 1];
+                            const new_past = past_data.splice(past_data.length - 1, 1);
+                            future_data.unshift(present_data);
+                            this.setState({ past_data: new_past, present_data: previous, future_data: future_data });
+                        }
                     }}>Undo</Button>
                     <Button bsStyle="primary" onClick={() => {
-                        console.log(this.state.data_history);
+                        if (this.state.future_data.length > 0) {
+                            const { past_data, present_data, future_data } = JSON.parse(JSON.stringify(this.state));
 
+                            const next = future_data[0];
+                            const new_future = future_data.splice(0, 1);
+                            past_data.push(present_data);
+                            this.setState({ past_data: past_data, present_data: next, future_data: new_future });
+                        }
                     }}>Redo</Button>
                 </Row>
             </div>
